1837b29efb84298145e8b921d6df6e88875ef4b7
ToruNagi <u052ff@yamaguchi-u.ac.jp>
2016-09-18 15:51:51 +0900

    [update]map.rbで背景を扱えるように修正
    背景を読み込むがインスタンスを生成するとき引数がなければ一度に読み込む。
    また、画像の縦横の大きさを返す

--- src/map.rb
+++ src/map.rb
@@ -1,2 +1,30 @@
 class Map
+
+	#これに全８マップを格納する
+	attr_accessor :map
+
+	def initialize(num=nil)
+		self.map = []
+		if (num != nil)
+			map << Image.load(("images/map/stage" + num.to_s + ".jpg"))
+		else
+			read_map
+		end
+	end
+
+	#背景を一度に読み込む関数
+	def read_map
+		8.times do |i|
+			map << Image.load(("images/map/stage" + (i + 1).to_s + ".jpg"))
+		end
+	end
+
+	def width
+		return map.first.width
+	end
+
+	def height
+		return map.first.height
+	end
+	
 end
