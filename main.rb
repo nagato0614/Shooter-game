fe3e122d5509109d1246cea2e966e2acaeddfc4a
ToruNagi <u052ff@yamaguchi-u.ac.jp>
2016-09-19 20:25:50 +0900

    [update]fpsを左上に表示している。
    player_baseのdelete関数のテストのために一時的に追加している。

--- main.rb
+++ main.rb
@@ -4,14 +4,19 @@ require 'dxruby'
 Window.width = 800
 Window.height = 600
 
+require_relative 'src/player_base'
 require_relative 'src/map'
 require_relative 'scene'
 require_relative 'load_scenes'
 
 Scene.set_current_scene(:title)
 
+font = Font.new(15, "MS 明朝")
+
 Window.loop do 
 	break if Input.keyPush?(K_ESCAPE)
 
 	Scene.play_scene
+
+	Window.draw_font(0, 0, Window.real_fps.to_s, font)
 end
