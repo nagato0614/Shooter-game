7be6872723351e10131a69a9bafe32b074356c61
ToruNagi <u052ff@yamaguchi-u.ac.jp>
2016-10-13 22:02:30 +0900

    [update]clear画面を追加

--- scenes/ending/director.rb
+++ scenes/ending/director.rb
@@ -1,11 +1,14 @@
 module Ending
 	class Director
 
-		def initializ
+		attr_accessor :image, :render
+
+		def initialize
+			@font = Font.new(75, "MS 明朝")
 		end
 
 		def play
-
+			Window.draw_font(300, 200, "CLEAR", @font)
 		end
 		
 	end
